ff2fb68626d60c6696dec89f811659897d28cfa6
vikashthakur-dev <vikash753055@gmail.com>
2025-03-25 00:08:30 +0530

    all code push

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:play/screens/atext.dart';
+import 'package:play/screens/bookdetail.dart';
+import 'package:play/screens/doctor.dart';
+import 'package:play/screens/ginger.dart';
+import 'package:play/screens/help.dart';
+import 'package:play/screens/profile.dart';
+import 'package:play/screens/stack.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: stacks(),
+    );
+  }
+}
